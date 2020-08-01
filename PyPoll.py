edfda00dd431f77e287ceb7855afa639d6fb4fa7
Meziane Kechabia <meziane228@gmail.com>
2020-07-31 20:34:04 -0700

    change the structure for Pypoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -16,9 +16,13 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-    # Read and print the header row.
-    headrs = next(file_reader)
-    print(headers)
+
+    # Read the header row.
+    headers = next(file_reader)    
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
+
 
 
 
